b57ce483dcbad80649109314560c2f78463b4497
campbell-frost <campbellsfrost@gmail.com>
2024-07-04 20:46:33 -0400

    added theme toggle

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,6 @@
 import AuthButton from "./AuthButton";
 import DeployButton from "./DeployButton";
+import ThemeToggle from "./ToggleTheme";
 
 export default function Nav(){
     return (
@@ -7,6 +8,7 @@ export default function Nav(){
           <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
             <DeployButton />
             <AuthButton />
+            <ThemeToggle />
           </div>
         </nav>
     )
---
+++ components/ToggleTheme.tsx
@@ -0,0 +1,21 @@
+'use client'
+import * as React from "react"
+import { Button } from "./ui/button";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes"
+
+export default function ThemeToggle() {
+    const { setTheme, resolvedTheme } = useTheme();
+    const dark = resolvedTheme === 'dark'
+
+    const handleThemeChange = () => {
+        dark ? setTheme('light') : setTheme('dark');
+    }
+
+    return (
+        <Button variant="outline" size="icon" onClick={handleThemeChange}>
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        </Button>
+    )
+}
