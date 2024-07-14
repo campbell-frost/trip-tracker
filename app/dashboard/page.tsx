84a1298cf2f7893aaa106b178dd19a730b753913
campbell-frost <campbellsfrost@gmail.com>
2024-07-13 23:00:38 -0400

    added condition when no trips have been created

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,25 +1,36 @@
-import { createClient } from "@/utils/supabase/server";
-import getTrips from "@/data/getTrips"
-import React, { Suspense } from "react"
-import { TripCard } from "../../components/TripCard";
-import DashboardSkeleton from "@/components/skeletons/DashboardSkeleton";
-import { checkAuth } from "@/data/checkAuth";
+import { createClient } from '@/utils/supabase/server';
+import getTrips from '@/data/getTrips';
+import React, { Suspense } from 'react';
+import { TripCard } from '../../components/TripCard';
+import DashboardSkeleton from '@/components/skeletons/DashboardSkeleton';
+import { checkAuth } from '@/data/checkAuth';
 
 async function TripsContent() {
   const trips = await getTrips();
   const supabase = await createClient();
   const { data: user } = await supabase.auth.getUser();
   checkAuth();
-  
+
   return (
     <div className="w-full min-h-screen bg-background py-8">
       <div className="max-w-4xl mx-auto px-4">
-        <h1 className="text-3xl font-bold mb-6 text-center text-foreground">Hi Legend {user.user?.email}</h1>
-        <div className="grid gap-6">
-          {trips.map((trip) => (
-            <TripCard key={trip.id} trip={trip} tripId={trip.id} tripName={trip.name!} />
-          ))}
-        </div>
+        <h1 className="text-3xl font-bold mb-6 text-center text-foreground">
+          Hi Legend {user.user?.email}
+        </h1>
+        {trips.length !== 0 ? (
+          <div className="grid gap-6">
+            {trips.map((trip) => (
+              <TripCard key={trip.id} trip={trip} tripId={trip.id} tripName={trip.name!} />
+            ))}
+          </div>
+        ) : (
+          <>
+            <h1 className="text-3xl font-bold mb-6 text-center text-foreground">No trips found.</h1>
+            <h1 className="text-3xl font-bold mb-6 text-center text-foreground">
+              Start adding trips by clicking on the Add Trip button above
+            </h1>
+          </>
+        )}
       </div>
     </div>
   );
@@ -30,5 +41,5 @@ export default async function DashBoard() {
     <Suspense fallback={<DashboardSkeleton />}>
       <TripsContent />
     </Suspense>
-  )
+  );
 }
