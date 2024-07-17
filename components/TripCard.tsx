7a33a9ce29d35f6f313f8e43ef3a5a4c30a005c4
campbell-frost <campbellsfrost@gmail.com>
2024-07-17 19:59:03 -0400

    Fixed ESLint errors

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React from 'react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardTitle } from '@/components/ui/card';
 import { Tables } from '@/types/supabase';
