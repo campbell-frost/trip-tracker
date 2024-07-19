502f02717a4a632acee0c7e3ac40f0c2a6acf759
campbell-frost <campbellsfrost@gmail.com>
2024-07-18 21:48:08 -0400

    Removed unneeded comments

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -21,9 +21,7 @@ export async function createClient() {
               cookieStore.set(name, value, options)
             )
           } catch (error) {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
+
             console.log("server error: ", error);
           }
         },
